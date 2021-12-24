d4ecc759a045dc10a57733413c4c4fa2377dbf97
Haowen Zhang <hwzhang94@gmail.com>
2021-12-24 13:24:07 -0500

    Refactor the code of Index.
    1. Delete some unused code.
    2. Add some const.

--- src/index.h
+++ src/index.h
@@ -9,8 +9,6 @@
 #include "mapping_metadata.h"
 #include "sequence_batch.h"
 
-//#define LI_DEBUG
-
 namespace chromap {
 #define KHashFunctionForIndex(a) ((a) >> 1)
 #define KHashEqForIndex(a, b) ((a) >> 1 == (b) >> 1)
@@ -24,9 +22,10 @@ enum Direction {
 struct mmHit {
   uint32_t mi;
   uint64_t position;
+
   bool operator<(const mmHit &h) const {
-    return position >
-           h.position;  // the inversed direction is to make a min-heap
+    // the inversed direction is to make a min-heap
+    return position > h.position;
   }
 };
 
@@ -40,6 +39,7 @@ class Index {
         index_file_path_(index_file_path) {  // for read mapping
     lookup_table_ = kh_init(k64);
   }
+
   Index(int kmer_size, int window_size, int num_threads,
         const std::string &index_file_path)
       : kmer_size_(kmer_size),
@@ -48,36 +48,34 @@ class Index {
         index_file_path_(index_file_path) {  // for index construction
     lookup_table_ = kh_init(k64);
   }
-  ~Index() {
+
+  ~Index() { Destroy(); }
+
+  void Destroy() {
     if (lookup_table_ != NULL) {
       kh_destroy(k64, lookup_table_);
+      lookup_table_ = NULL;
     }
-  }
-  void Destroy() {
-    kh_destroy(k64, lookup_table_);
-    lookup_table_ = NULL;
+
     std::vector<uint64_t>().swap(occurrence_table_);
   }
-  khash_t(k64) const *GetLookupTable() const { return lookup_table_; }
-  int GetKmerSize() const { return kmer_size_; }
-  int GetWindowSize() const { return window_size_; }
-  uint32_t GetLookupTableSize() const { return kh_size(lookup_table_); }
-  std::vector<uint64_t> const &GetOccurrenceTable() const {
-    return occurrence_table_;
-  }
-  void Statistics(uint32_t num_sequences, const SequenceBatch &reference);
-  void CheckIndex(uint32_t num_sequences, const SequenceBatch &reference);
-  void GenerateMinimizerSketch(
-      const SequenceBatch &sequence_batch, uint32_t sequence_index,
-      std::vector<std::pair<uint64_t, uint64_t> > &minimizers);
+
   void Construct(uint32_t num_sequences, const SequenceBatch &reference);
-  void Save();
+
+  void Save() const;
+
   void Load();
-  void GenerateCandidatesOnOneDirection(
-      int error_threshold, int num_seeds_required, uint32_t num_minimizers,
-      std::vector<uint64_t> &hits, std::vector<Candidate> &candidates) const;
-  void GenerateCandidates(int error_threshold,
-                          MappingMetadata &mapping_metadata) const;
+
+  void Statistics(uint32_t num_sequences, const SequenceBatch &reference) const;
+
+  void CheckIndex(uint32_t num_sequences, const SequenceBatch &reference) const;
+
+  int CollectSeedHits(
+      int max_seed_frequency, int repetitive_seed_frequency,
+      const std::vector<std::pair<uint64_t, uint64_t> > &minimizers,
+      uint32_t &repetitive_seed_length, std::vector<uint64_t> &positive_hits,
+      std::vector<uint64_t> &negative_hits, bool use_heap) const;
+
   int GenerateCandidatesFromRepetitiveReadWithMateInfo(
       int error_threshold,
       const std::vector<std::pair<uint64_t, uint64_t> > &minimizers,
@@ -85,11 +83,24 @@ class Index {
       std::vector<Candidate> &candidates,
       std::vector<Candidate> &mate_candidates, Direction direction,
       uint32_t range) const;
-  int CollectCandidates(
-      int max_seed_frequency, int repetitive_seed_frequency,
-      const std::vector<std::pair<uint64_t, uint64_t> > &minimizers,
-      uint32_t &repetitive_seed_length, std::vector<uint64_t> &positive_hits,
-      std::vector<uint64_t> &negative_hits, bool use_heap) const;
+
+  int GetKmerSize() const { return kmer_size_; }
+
+  int GetWindowSize() const { return window_size_; }
+
+  uint32_t GetLookupTableSize() const { return kh_size(lookup_table_); }
+
+  void GenerateMinimizerSketch(
+      const SequenceBatch &sequence_batch, uint32_t sequence_index,
+      std::vector<std::pair<uint64_t, uint64_t> > &minimizers) const;
+
+  void GenerateCandidatesOnOneDirection(
+      int error_threshold, int num_seeds_required, uint32_t num_minimizers,
+      std::vector<uint64_t> &hits, std::vector<Candidate> &candidates) const;
+
+  void GenerateCandidates(int error_threshold,
+                          MappingMetadata &mapping_metadata) const;
+
   inline static uint64_t Hash64(uint64_t key, const uint64_t mask) {
     key = (~key + (key << 21)) & mask;  // key = (key << 21) - key - 1;
     key = key ^ key >> 24;
@@ -105,12 +116,18 @@ class Index {
   int kmer_size_;
   int window_size_;
   int min_num_seeds_required_for_mapping_;
+  // Vector of size 2. The first element is the frequency threshold, and the
+  // second element is the frequency threshold to run rescue. The second element
+  // should always larger than the first one.
+  // TODO(Haowen): add an error check.
   std::vector<int> max_seed_frequencies_;
+  // Number of threads to build the index, which is not used right now.
   int num_threads_;
-  std::string index_file_path_;
+  const std::string index_file_path_;
   khash_t(k64) *lookup_table_ = NULL;
   std::vector<uint64_t> occurrence_table_;
 };
+
 }  // namespace chromap
 
 #endif  // INDEX_H_
