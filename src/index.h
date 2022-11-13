4498d61900996e7ab478049fee1014f75f299bf6
Swift Genomics <swiftgenomics@outlook.com>
2022-11-13 03:42:38 -0500

    Rename variables.

--- src/index.h
+++ src/index.h
@@ -81,8 +81,8 @@ class Index {
   uint32_t GetLookupTableSize() const { return kh_size(lookup_table_); }
 
  private:
-  uint64_t GenerateCandidatePositionFromHits(uint64_t reference_seed_hit,
-                                             uint64_t read_seed_hit) const;
+  uint64_t GenerateCandidatePositionFromHits(uint64_t reference_hit,
+                                             uint64_t read_hit) const;
 
   void UpdateRepetitiveSeedStats(uint32_t read_position,
                                  RepetitiveSeedStats &stats) const;
